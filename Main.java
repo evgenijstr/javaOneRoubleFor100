2ce9ebcf9c23f93860dd36bbc1d6c59f8b9382fa
Evgenijstr <evgenijstr@gmail.com>
2021-08-16 22:34:21 +0300

    Bonus calculation added

--- Main.java
+++ Main.java
@@ -1,15 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         int initialAmount = 100;
-        int minIncomeAmount = 5000;
-        double bonus;
-        if (minIncomeAmount > 1000) {
-            bonus = 0.01;
+        int incomeAmount = 1100;
+        double coefficient;
+        int bonus;
+        if (incomeAmount > 1000) {
+            coefficient = 0.01;
+        } else {
+            coefficient = 0;
+        }
+        int totalAmount = (int) (initialAmount + incomeAmount + incomeAmount * coefficient);
+        if (incomeAmount > 1000) {
+            bonus = (int) (incomeAmount * coefficient);
         } else {
             bonus = 0;
         }
-        int totalAmount = (int) (initialAmount + minIncomeAmount + minIncomeAmount * bonus);
-
         System.out.println(totalAmount);
-     }
+        System.out.println(bonus);
     }
+}
