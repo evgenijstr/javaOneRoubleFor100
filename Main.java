d6312adb8d03b89ef44b4dd5b60de2a12f28cf36
Evgenijstr <evgenijstr@gmail.com>
2021-08-24 22:12:20 +0300

    Main.java modified

--- Main.java
+++ Main.java
@@ -2,19 +2,14 @@ public class Main {
     public static void main(String[] args) {
         int initialAmount = 100;
         int incomeAmount = 1100;
-        double coefficient;
-        int bonus;
+        double bonus;
         if (incomeAmount > 1000) {
-            coefficient = 0.01;
-        } else {
-            coefficient = 0;
-        }
-        int totalAmount = (int) (initialAmount + incomeAmount + incomeAmount * coefficient);
-        if (incomeAmount > 1000) {
-            bonus = (int) (incomeAmount * coefficient);
+            bonus = incomeAmount * 0.01;
         } else {
             bonus = 0;
         }
+        int totalAmount = (int) (initialAmount + incomeAmount + bonus);
+
         System.out.println(totalAmount);
         System.out.println(bonus);
     }
